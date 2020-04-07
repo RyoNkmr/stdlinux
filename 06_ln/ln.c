89e7b58f8d0ac3ac6dfed57b693ade613d6b4a6b
RyoNkmr <ryonakamuraryo@gmail.com>
2020-04-08 01:14:29 +0900

    add ln -s

--- 06_ln/ln.c
+++ 06_ln/ln.c
@@ -2,8 +2,38 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#define _GNU_SOURCE
+#include <getopt.h>
+
+static struct option longopts[] = {
+  { "symbolic", no_argument, NULL, 's' },
+  {0,0,0,0},
+};
+
+#define ERR_TEMPLATE "Usage: %s [-s] [FILE ...]\n"
+
 int main(int argc, char const* argv[])
 {
+  int opt;
+
+  while ((opt = getopt_long(argc, argv, "s", longopts, NULL)) != -1) {
+    switch (opt) {
+      case 's': {
+                  if (symlink(argv[1], argv[2]) < 0) {
+                    perror(argv[1]);
+                    return 1;
+                  }
+                  return 0;
+                }
+      case 'h':
+        fprintf(stdout, ERR_TEMPLATE, argv[0]);
+        exit(0);
+      case '?':
+        fprintf(stderr, ERR_TEMPLATE, argv[0]);
+        exit(1);
+    }
+  }
+
   if (argc != 3) {
     fprintf(stderr, "%s: wrong arguments\n", argv[0]);
     return 1;
