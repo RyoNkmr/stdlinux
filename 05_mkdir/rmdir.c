9a82ee5fb20d9cc0ec86856171f69a86cc37c088
RyoNkmr <ryonakamuraryo@gmail.com>
2020-04-08 00:59:51 +0900

    mkdir, rmdir, ln

---
+++ 05_mkdir/rmdir.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char const* argv[])
+{
+  int i;
+
+  if (argc < 2) {
+    fprintf(stderr, "%s: no arguments\n", argv[0]);
+    return 1;
+  }
+
+  for (int i = 1; i < argc; i++) {
+    if (rmdir(argv[i]) < 0) {
+      perror(argv[i]);
+      return 1;
+    }
+  }
+  return 0;
+}
