f353c5b344eae078349c41e2d5bbaf14db1071c1
RyoNkmr <ryonakamuraryo@gmail.com>
2020-04-22 20:44:06 +0900

    cmd impl

--- 10_practices2/resessh.c
+++ 10_practices2/resessh.c
@@ -6,13 +6,22 @@
 #include <sys/wait.h>
 #include <errno.h>
 
-#define ARGV_SIZE 256
+#define CMD_SIZE 8
+#define ARGV_SIZE 32
 #define CHUNK_SIZE 256
 
+typedef struct {
+ char **argv;
+ int argc;
+} cmd;
+
 static void read_stdin(void);
-static void print_cwd(void);
+static void print_cwd(int is_fullpath, int brackets);
+static void print_re(void);
 static char* get_cwd(char *dest);
-static int exec(int argc, char* argv[]);
+static int exec(cmd *cmds);
+
+static void bye(void);
 static void cd(char *path);
 
 static char *env_home;
@@ -27,23 +36,31 @@ int main(int argc, char const* argv[])
   }
 
   while(1) {
-    print_cwd();
+    puts("");
+    print_cwd(1, 1);
+    print_re();
     read_stdin();
   }
   return 0;
 }
 
 static void read_stdin(void) {
+  cmd *cmds;
+  int cmd_size = CMD_SIZE;
+  cmds = (cmd *)calloc(cmd_size, sizeof(cmd));
+  int cmdc = 0;
+
   int argv_size = ARGV_SIZE;
   char **argv;
   argv = (char **)calloc(argv_size, sizeof(char));
   int argc = 0;
-  int ch;
 
   int chunk_size = CHUNK_SIZE;
   int chunk_ptr_index = 0;
   char *chunk;
   chunk = (char *)calloc(chunk_size, sizeof(char));
+
+  int ch;
 
   while(1) {
     ch = getchar();
@@ -84,8 +101,26 @@ static void read_stdin(void) {
       continue;
     }
 
+    if (ch == '|' || ch == '\n') {
+      cmds[cmdc].argv = argv;
+      cmds[cmdc].argc = argc;
+
+      argv_size = ARGV_SIZE;
+      argv = (char **)calloc(argv_size, sizeof(char));
+      argc = 0;
+
+      free(chunk);
+      chunk_size = CHUNK_SIZE;
+      chunk = (char *)calloc(chunk_size, sizeof(char));
+      chunk_ptr_index = 0;
+    }
+
+    if (ch == '|') {
+      continue;
+    }
+
     if (ch == '\n') {
-      exec(argc, argv);
+      exec(cmds);
       break;
     }
 
@@ -107,29 +142,43 @@ static void read_stdin(void) {
     chunk_ptr_index++;
   }
 
-  for (int i = 0; i < argv_size ; i++) {
-    if (argv[i] == NULL) {
-      break;
+  for (int i = 0; i < cmdc; i++) {
+    for (int j = 0; j < cmds[i].argc; j++) {
+      if (cmds[i].argv[j] == NULL) {
+        break;
+      }
+      free(cmds[i].argv[j]);
     }
-    free(argv[i]);
+    free(cmds[i].argv);
   }
-  free(argv);
+  free(cmds);
   free(chunk);
 }
 
-static int exec(int argc, char* argv[]) {
+static int exec(cmd* cmds) {
+  char **argv = cmds[0].argv;
+  int argc = cmds[0].argc;
+
+  if (strcmp(argv[0], "pwd") == 0) {
+    if (argc > 1) {
+      fprintf(stderr, "%s: too many argments\n", argv[0]);
+      return 1;
+    }
+    print_cwd(0, 0);
+    return 0;
+  }
+
   if (strcmp(argv[0], "cd") == 0) {
     if (argc != 2) {
       fprintf(stderr, "%s: wrong argments\n", argv[0]);
-      exit(1);
+      return 1;
     }
     cd(argv[1]);
     return 0;
   }
 
-  if (strcmp(argv[0], "exit") == 0) {
-    printf("resessh says goodbye to you👋\n");
-    exit(0);
+  if (strcmp(argv[0], "exit") == 0 || strcmp(argv[0], "bye") == 0) {
+    bye();
   }
 
   pid_t pid = fork();
@@ -146,17 +195,12 @@ static int exec(int argc, char* argv[]) {
 
   int status;
   waitpid(pid, &status, 0);
-  /* printf("child (PID=%d) exited;", pid); */
-  /*  */
-  /* if (WIFEXITED(status)) { */
-  /*   printf("exit, status=%d\n", WEXITSTATUS(status)); */
-  /* } else if (WIFSIGNALED(status)) { */
-  /*   printf("signal, signal=%d\n", WTERMSIG(status)); */
-  /* } else { */
-  /*   printf("abnormal exit\n"); */
-  /* } */
-  /*  */
   return 0;
+}
+
+static void bye() {
+  printf("resessh says goodbye to you👋\n");
+  exit(0);
 }
 
 static char* _getcwd() {
@@ -191,10 +235,10 @@ static char* _getcwd() {
   return ptr;
 }
 
-static void print_cwd() {
+static void print_cwd(int is_fullpath, int brackets) {
   char *name = _getcwd();
 
-  if (env_home != NULL) {
+  if (env_home != NULL && !is_fullpath) {
     char *ptr;
     if ((ptr = strstr(name, env_home)) == name) {
       int len = strlen(ptr) - env_home_len + 1;
@@ -210,8 +254,12 @@ static void print_cwd() {
     }
   }
 
-  printf("[%s]\n李: ", name);
+  printf(brackets ? "[%s]\n" : "%s\n", name);
   free(name);
+}
+
+static void print_re() {
+  printf("李: ");
 }
 
 static void cd(char *path) {
