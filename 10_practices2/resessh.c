80a446512b2d68c131e7d1424122d32ad90c761c
RyoNkmr <ryonakamuraryo@gmail.com>
2020-04-25 00:56:24 +0900

    pipe

--- 10_practices2/resessh.c
+++ 10_practices2/resessh.c
@@ -10,16 +10,20 @@
 #define ARGV_SIZE 32
 #define CHUNK_SIZE 256
 
+#define PIPE_READ 0
+#define PIPE_WRITE 1
+
 typedef struct {
  char **argv;
  int argc;
 } cmd;
 
 static void read_stdin(void);
-static void print_cwd(int is_fullpath, int brackets);
+static void pwd(int is_fullpath, int brackets);
 static void print_re(void);
 static char* get_cwd(char *dest);
-static int exec(int cmdc, cmd *cmds);
+static int exec_cmds(int cmdc, cmd *cmds);
+static void exec(int argc, char *argv[]);
 
 static void bye(void);
 static void cd(char *path);
@@ -37,7 +41,7 @@ int main(int argc, char const* argv[])
 
   while(1) {
     puts("");
-    print_cwd(1, 1);
+    pwd(1, 1);
     print_re();
     read_stdin();
   }
@@ -69,8 +73,10 @@ static void read_stdin(void) {
       exit(1);
     }
 
-    if (ch == ' ' && chunk[chunk_ptr_index] == ' ') {
-      continue;
+    if (ch == ' ') {
+      if (chunk_ptr_index == 0 || chunk[chunk_ptr_index] == ' ') {
+        continue;
+      }
     }
 
     if (ch == ' ' || ch == '\n') {
@@ -121,7 +127,7 @@ static void read_stdin(void) {
     }
 
     if (ch == '\n') {
-      exec(cmdc, cmds);
+      exec_cmds(cmdc, cmds);
       break;
     }
 
@@ -156,47 +162,118 @@ static void read_stdin(void) {
   free(chunk);
 }
 
-static int exec(int cmdc, cmd* cmds) {
+static int exec_cmds(int cmdc, cmd* cmds) {
   char **argv = cmds[0].argv;
   int argc = cmds[0].argc;
-
-  if (strcmp(argv[0], "pwd") == 0) {
-    if (argc > 1) {
-      fprintf(stderr, "%s: too many argments\n", argv[0]);
-      return 1;
-    }
-    print_cwd(0, 0);
-    return 0;
-  }
-
-  if (strcmp(argv[0], "cd") == 0) {
-    if (argc != 2) {
-      fprintf(stderr, "%s: wrong argments\n", argv[0]);
-      return 1;
-    }
-    cd(argv[1]);
-    return 0;
-  }
+  pid_t *pids = (pid_t *)calloc(cmdc, sizeof(pid_t));
 
   if (strcmp(argv[0], "exit") == 0 || strcmp(argv[0], "bye") == 0) {
     bye();
   }
 
-  pid_t pid = fork();
-  if (pid < 0) {
-    fprintf(stderr, "fork(2) failed\n");
+  int original_stdinout[2];
+  original_stdinout[PIPE_READ] = dup(STDIN_FILENO);
+  original_stdinout[PIPE_WRITE] = dup(STDOUT_FILENO);
+
+  int prev_pipe[2];
+  if (pipe(prev_pipe) < 0) {
+    perror("pipe");
     exit(1);
   }
+  int current_pipe[2] = {-1, -1};
 
-  if (pid == 0) {
-    execvp(argv[0], argv);
-    perror(argv[0]);
-    exit(99);
+  for (int i = 0; i < cmdc; i++) {
+    if (pipe(current_pipe) < 0) {
+      perror("pipe");
+      close(prev_pipe[PIPE_READ]);
+      close(prev_pipe[PIPE_WRITE]);
+      exit(1);
+    }
+
+    pids[i] = fork();
+
+    if (pids[i] < 0) {
+      fprintf(stderr, "fork(2) failed\n");
+      close(prev_pipe[PIPE_READ]);
+      close(prev_pipe[PIPE_WRITE]);
+      close(current_pipe[PIPE_READ]);
+      close(current_pipe[PIPE_WRITE]);
+      _exit(1);
+    }
+
+    if (pids[i] == 0) {
+      /* child */
+      if (i > 0) {
+        close(STDIN_FILENO);
+        dup2(prev_pipe[PIPE_READ], STDIN_FILENO);
+        close(prev_pipe[PIPE_READ]);
+        close(prev_pipe[PIPE_WRITE]);
+      }
+
+      if (i < cmdc - 1) {
+        close(current_pipe[PIPE_READ]);
+        close(STDOUT_FILENO);
+        dup2(current_pipe[PIPE_WRITE], STDOUT_FILENO);
+        close(current_pipe[PIPE_WRITE]);
+      }
+
+      exec(cmds[i].argc, cmds[i].argv);
+      perror("exec");
+      _exit(99);
+    }
+
+    /* parent */
+    close(prev_pipe[PIPE_READ]);
+    close(prev_pipe[PIPE_WRITE]);
+    if (i > 0) {
+      close(current_pipe[PIPE_WRITE]);
+      close(current_pipe[PIPE_READ]);
+    }
+
+    prev_pipe[PIPE_READ] = current_pipe[PIPE_READ];
+    prev_pipe[PIPE_WRITE] = current_pipe[PIPE_WRITE];
   }
 
+  close(STDIN_FILENO);
+  dup2(original_stdinout[PIPE_READ], STDIN_FILENO);
+  close(original_stdinout[PIPE_READ]);
+  close(STDOUT_FILENO);
+  dup2(original_stdinout[PIPE_WRITE], STDOUT_FILENO);
+  close(original_stdinout[PIPE_WRITE]);
+
   int status;
-  waitpid(pid, &status, 0);
+  for (int i = 0; i < cmdc; i++) {
+    waitpid(pids[i], &status, 0);
+    if (!WIFEXITED(status) && !WIFSIGNALED(status)) {
+      return -1;
+    }
+  }
+
   return 0;
+}
+
+static void exec(int argc, char* argv[]) {
+  if (strcmp(argv[0], "pwd") == 0) {
+    if (argc > 1) {
+      fprintf(stderr, "%s: too many argments\n", argv[0]);
+      _exit(1);
+    }
+    pwd(0, 0);
+    _exit(0);
+  }
+
+  if (strcmp(argv[0], "cd") == 0) {
+    if (argc != 2) {
+      fprintf(stderr, "%s: wrong argments\n", argv[0]);
+      _exit(1);
+    }
+    cd(argv[1]);
+    _exit(0);
+  }
+
+  execvp(argv[0], argv);
+  perror(argv[0]);
+  _exit(99);
 }
 
 static void bye() {
@@ -236,9 +313,8 @@ static char* _getcwd() {
   return ptr;
 }
 
-static void print_cwd(int is_fullpath, int brackets) {
+static void pwd(int is_fullpath, int brackets) {
   char *name = _getcwd();
-
   if (env_home != NULL && !is_fullpath) {
     char *ptr;
     if ((ptr = strstr(name, env_home)) == name) {
@@ -254,8 +330,7 @@ static void print_cwd(int is_fullpath, int brackets) {
       name = _name;
     }
   }
-
-  printf(brackets ? "[%s]\n" : "%s\n", name);
+  printf(brackets ?  "[%s]\n" : "%s\n", name);
   free(name);
 }
 
